e79be569b9cf4bbaab531e06d8c7dc7b3529d1b2
Tod Kurt <todbot@users.noreply.github.com>
2021-05-22 19:35:08 -0700

    actual working hsv_to_rgb()

--- ledtools.py
+++ ledtools.py
@@ -82,6 +82,7 @@ class LEDTools:
         """ 
         Convert H,S,V in 0-255,0-255,0-255 format
         to R,G,B in 0-255,0-255,0-255 format
+        (this is currently busted)
         """
         import math
         h = h/255
@@ -103,3 +104,29 @@ class LEDTools:
         elif hi == 5: r, g, b = v, p, q
         r, g, b = int(r * 255), int(g * 255), int(b * 255)
         return r, g, b
+
+
+    # stolen from     
+    # https://github.com/wntrblm/Sol/blob/master/firmware/winterbloom_sol/_utils.py    
+    def hsv_to_rgb(h, s, v):
+        """ 
+        Convert H,S,V in 0-255,0-255,0-255 format
+        to R,G,B in 0-255,0-255,0-255 format
+        """
+        if s == 0: return v, v, v
+        h = h/255
+        s = s/255
+        v = v/255
+        i = int(h * 6.0)  # XXX assume int() truncates!
+        f = (h * 6.0) - i
+        p = v * (1.0 - s)
+        q = v * (1.0 - s * f)
+        t = v * (1.0 - s * (1.0 - f))
+        i = i % 6
+        if i == 0: r = v; g = t; b = p
+        if i == 1: r = q; g = v; b = p
+        if i == 2: r = p; g = v; b = t
+        if i == 3: r = p; g = q; b = v
+        if i == 4: r = t; g = p; b = v
+        if i == 5: r = v; g = p; b = q
+        return int(r*255), int(g*255), int(b*255)
